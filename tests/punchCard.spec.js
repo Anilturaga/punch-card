067ff5f4652e09a66ab562813d0ba077178255de
AnilTuraga <31271852+Anilturaga@users.noreply.github.com>
2022-09-20 15:56:17 +0530

    printing

--- tests/punchCard.spec.js
+++ tests/punchCard.spec.js
@@ -3,7 +3,8 @@ const { test, expect } = require("@playwright/test");
 
 test("PunchCard", async ({ page }) => {
   await page.goto(process.env.URL);
-  console.log(process.env.URL)
+  console.log("PunchCard");
+  console.log(process.env.URL.length);
   // Expect a title "to contain" a substring.
   await page.locator("#username").fill(process.env.USR);
   // await new Promise(r => setTimeout(r, 2000)); // hard wait for 2000ms
