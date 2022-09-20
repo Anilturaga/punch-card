c7a9fb02a9e478647f8f66ffb1e905cc7904b378
AnilTuraga <31271852+Anilturaga@users.noreply.github.com>
2022-09-20 17:15:55 +0530

    bug fixes and cleanup

--- tests/punchCard.spec.js
+++ tests/punchCard.spec.js
@@ -4,30 +4,22 @@ const { test, expect } = require("@playwright/test");
 test("PunchCard", async ({ page }) => {
   await page.goto(process.env.URL);
   console.log("PunchCard");
-  console.log(process.env.URL.length, process.env.USR.length, process.env.PASS.length);
-  const PASSr = process.env.PASS.replace("@", "aa");
-  console.log(PASSr);
   // Expect a title "to contain" a substring.
   await page.locator("#username").fill(process.env.USR);
   // await new Promise(r => setTimeout(r, 2000)); // hard wait for 2000ms
   await page.locator("#password").fill(process.env.PASS);
 
-  await new Promise(r => setTimeout(r, 1000)); // hard wait for 2000ms
+  await new Promise(r => setTimeout(r, 1000)); // hard wait for 1000ms
   await page.click("text=Log in");
   console.log("Logging in");
   var d = new Date();
-  // print date & time in YYYY-MM-DD HH:MM:SS format
   console.log(d.toLocaleString());
-  // console.log("Hours: " + d.getHours());
   
-  // await page.waitForURL('process.env.URL');
   if (d.getHours()+5 < 12) {
     console.log("Morning");
     await page.click("text=Sign In");
   } else {
     console.log("Afternoon");
-    const url=await page.url();
-    console.log(url);
     await page.click("text=Sign Out");
   }
   console.log("Done");
